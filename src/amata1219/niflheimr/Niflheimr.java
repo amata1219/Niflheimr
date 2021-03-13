9c20039dd44255675e1c303d4548488a148b4460
amata1219 <suzukiamata@gmail.com>
2021-03-13 15:38:24 +0900

    プラグインの無効化時にインベントリUIを強制的に閉じる処理を追加

--- src/amata1219/niflheimr/Niflheimr.java
+++ src/amata1219/niflheimr/Niflheimr.java
@@ -3,10 +3,12 @@ package amata1219.niflheimr;
 import amata1219.niflheimr.enchantment.GleamEnchantment;
 import amata1219.niflheimr.dsl.InventoryOperationListener;
 import org.bukkit.enchantments.Enchantment;
+import org.bukkit.entity.Player;
 import org.bukkit.event.HandlerList;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import java.lang.reflect.Field;
+import java.util.Objects;
 
 public class Niflheimr extends JavaPlugin {
 
@@ -24,7 +26,7 @@ public class Niflheimr extends JavaPlugin {
             Enchantment.registerEnchantment(GleamEnchantment.INSTANCE);
             acceptingNew.set(null, false);
             acceptingNew.setAccessible(false);
-        } catch (NoSuchFieldException | IllegalAccessException e) {
+        } catch (NoSuchFieldException | IllegalAccessException ignored) {
 
         }
 
@@ -33,6 +35,11 @@ public class Niflheimr extends JavaPlugin {
 
     @Override
     public void onDisable() {
+        for (Player player : getServer().getOnlinePlayers()) {
+            if (InventoryOperationListener.tryExtractInventoryLayout(player.getInventory()) != null)
+                player.closeInventory();
+        }
+
         HandlerList.unregisterAll(this);
     }
 
